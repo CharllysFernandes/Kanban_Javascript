e31f52412da63a72c66e7d3d7b856f38145780fa
Charllys Fernandes <charllys.daiany@gmail.com>
2023-01-13 19:21:55 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 let btnAddTask = document.getElementById('addTask');
 let inputAddTask = document.getElementById('inputAddTask');
-let bodyNewTask = document. getElementById('bodyNewTask');
-let dadosJSON = [];
+let bodyNewTask = document.getElementById('bodyNewTask');
+let clickCard = document.getElementById('card');
+let dadosJSON = new Array()
 
 btnAddTask.addEventListener('click', function () {
-   inputAddTask.value == "" ? alert('Add new task') : addNewTask()
+    inputAddTask.value == "" ? alert('Add new task') : addNewTask();
 
 });
 
+clickCard.addEventListener('click', function () {
+    console.log('Cores')
+    document.getElementById('colorId').classList.remove('d-none');
+
+})
+
 function addNewTask() {
-    bodyNewTask.innerHTML += 
-    `
-    <div class="card bg-dark-subtle p-1 border-start text-dark my-2">
-        <span>${inputAddTask.value}</span>
+    let taskInput = inputAddTask.value;
+
+    bodyNewTask.innerHTML +=
+        `
+    <div class="card bg-dark-subtle p-1 border-start text-dark my-2" >
+        <span>${taskInput}</span>
     </div>
     `
     inputAddTask.value = ""
+    dadosJSON.push({ task: `${taskInput}`, column: "new task" })
+    console.log(dadosJSON)
+    saveData(dadosJSON)
 }
+
+function saveData(data) {
+    let dataDB = JSON.stringify(data)
+    localStorage.setItem("dataDB", dataDB)
+}
+
