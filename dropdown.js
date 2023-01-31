c040458243045fb3c3e84e0e9de61e31296682c9
Charllys Fernandes <charllys.daiany@gmail.com>
2023-01-31 00:01:06 -0300

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -1,9 +1,14 @@
 for (let indexDatabase = 0; indexDatabase < database.length; indexDatabase++) {
-    console.log(indexDatabase)
+    let tasksFromOneIndex = database[indexDatabase] 
     for (let indexTask = 0; indexTask < database[indexDatabase].task.length; indexTask++) {
        idDropMenu = "dropdown-menu"+indexDatabase+indexTask
         dropdownMenu = document.getElementById(idDropMenu)
-        dropdownMenu.innerHTML += 'TESTE'        
+        // dropdownMenu.innerHTML += 'TESTE'
+        
+        console.log(tasksFromOneIndex)
+        
+
+
     }
     
 }
